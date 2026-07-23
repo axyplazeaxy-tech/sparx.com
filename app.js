3ec0761a305f1f8bb7cf556d815405fc33825e48
axyplazeaxy-tech <axyplazeaxy@gmail.com>
2026-07-23 19:39:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -85,7 +85,6 @@ function openPage(page){
         Science Department<br>
         Student Support
         </p>
-
         `;
 
     }
@@ -101,7 +100,6 @@ function openPage(page){
         Email: school@example.com<br>
         Phone: 0000 000000
         </p>
-
         `;
 
     }
@@ -115,47 +113,270 @@ function openPage(page){
 
 
 
+
 // RETRO GAME AREA
 
 
 function openGames(){
 
 
-    document.body.innerHTML = `
+document.body.innerHTML = `
 
-    <div id="app">
-
-
-    <header>
-
-    <h1>🎮 School Arcade</h1>
-
-    <p>Learn more by having fun!</p>
-
-    </header>
+<div id="app">
 
 
+<header>
 
-    <button onclick="location.reload()">
-    ← Back to School
-    </button>
+<h1>🎮 School Arcade</h1>
+
+<p>Retro games zone</p>
+
+</header>
 
 
 
-    <div class="card">
-
-    <h2>🕹️ Coming Soon</h2>
-
-    <p>
-    Retro games will be added here.
-    </p>
-
-    </div>
+<button onclick="location.reload()">
+← Back to School
+</button>
 
 
-    </div>
 
-    `;
+<h2>Choose a game</h2>
+
+
+<button onclick="startSnake()">
+🐍 Snake
+</button>
+
+
+
+<canvas 
+id="gameCanvas"
+width="400"
+height="400">
+</canvas>
+
+
+</div>
+
+`;
+
+
+}
+
+
+
+
+
+
+
+
+
+// SNAKE GAME
+
+
+function startSnake(){
+
+
+let canvas =
+document.getElementById("gameCanvas");
+
+
+let ctx =
+canvas.getContext("2d");
+
+
+
+let snake = [
+{
+x:200,
+y:200
+}
+];
+
+
+
+let food = {
+
+x:100,
+
+y:100
+
+};
+
+
+
+let dx = 20;
+
+let dy = 0;
+
+
+
+document.onkeydown = function(e){
+
+
+if(e.key === "ArrowUp"){
+
+dx = 0;
+dy = -20;
+
+}
+
+
+if(e.key === "ArrowDown"){
+
+dx = 0;
+dy = 20;
+
+}
+
+
+if(e.key === "ArrowLeft"){
+
+dx = -20;
+dy = 0;
+
+}
+
+
+if(e.key === "ArrowRight"){
+
+dx = 20;
+dy = 0;
+
+}
+
+
+};
+
+
+
+
+
+
+function loop(){
+
+
+
+ctx.fillStyle="black";
+
+ctx.fillRect(
+0,
+0,
+400,
+400
+);
+
+
+
+
+ctx.fillStyle="lime";
+
+
+snake.forEach(part=>{
+
+
+ctx.fillRect(
+part.x,
+part.y,
+20,
+20
+);
+
+
+});
+
+
+
+
+
+ctx.fillStyle="red";
+
+
+ctx.fillRect(
+food.x,
+food.y,
+20,
+20
+);
+
+
+
+
+
+
+let head = {
+
+x: snake[0].x + dx,
+
+y: snake[0].y + dy
+
+};
+
+
+
+
+snake.unshift(head);
+
+
+
+
+if(
+head.x === food.x &&
+head.y === food.y
+){
+
+
+food.x =
+Math.floor(Math.random()*20)*20;
+
+
+food.y =
+Math.floor(Math.random()*20)*20;
+
+
+}
+
+else{
+
+
+snake.pop();
+
+
+}
+
+
+
+
+
+if(
+head.x < 0 ||
+head.y < 0 ||
+head.x >= 400 ||
+head.y >= 400
+){
+
+
+alert("Game Over!");
+
+location.reload();
+
+
+return;
+
+
+}
+
+
+
+requestAnimationFrame(loop);
+
+
+}
+
+
+
+loop();
 
 
 }
