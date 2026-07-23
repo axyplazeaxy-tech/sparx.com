969289cd98b811c37470c4128e1ad8e154bbe805
axyplazeaxy-tech <axyplazeaxy@gmail.com>
2026-07-23 17:16:11 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function openGames() {
+    document.getElementById("arcade").style.display = "flex";
+}
+
+function closeGames() {
+    document.getElementById("arcade").style.display = "none";
+}
+
+function game(name) {
+    alert(name + " is coming soon!");
+}
